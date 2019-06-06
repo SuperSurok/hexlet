8d3b84e70184d238011a781d4e1b205bc1059cd5
Lebedev Sergey <oddyone@mail.ru>
2019-06-06 18:13:01 +0300

    Reverse string refactoring. Part 2.

--- programming_intro/reverse_string.js
+++ programming_intro/reverse_string.js
@@ -7,8 +7,8 @@
 // reverseInt(8900); // 98
 
 
-const reverseInt = (str) => {
-    const numberString = str.toString();
+const reverseInt = (num) => {
+    const numberString = num.toString();
     let start = '';
     if (numberString[0] === '-') {
         start = numberString.slice(0, 1);
@@ -24,3 +24,23 @@ const reverseInt = (str) => {
 };
 
 reverseInt(3100);
+
+/////////////////////////////////////////////////////////
+//////////////////// Second Solution ////////////////////
+/////////////////////////////////////////////////////////
+
+const reverseIntSecondSolution = (num) => {
+    const numberString = String(Math.abs(num));
+    let newString = '';
+    let i = numberString.length - 1;
+
+    while (i >= 0) {
+        newString += numberString[i];
+        i -= 1;
+    }
+
+    const reverseNumber = Number(newString);
+    return num < 0 ? -reverseNumber : reverseNumber;
+};
+
+reverseIntSecondSolution(3100);
