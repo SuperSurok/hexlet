4e604791ca7c9166a4699d50e7e692f76b337976
Lebedev Sergey <oddyone@mail.ru>
2019-06-06 17:25:17 +0300

    Reverse string.

---
+++ programming_intro/reverse_string.js
@@ -0,0 +1,27 @@
+// Реализуйте и экспортируйте по умолчанию функцию reverseInt,
+// которая переворачивает цифры в переданном числе и возвращает новое число.
+
+
+// reverseInt(13); // 31
+// reverseInt(-123); // -321
+// reverseInt(8900); // 98
+
+
+const reverseInt = (str) => {
+    const numberString = str.toString();
+    let start = '';
+    if (numberString[0] === '-') {
+        start = numberString.slice(0, 1);
+        for (let i = numberString.length - 1; i > 0; i -= 1) {
+            start += numberString[i];
+        }
+    } else {
+        for (let i = numberString.length - 1; i >= 0; i -= 1) {
+            start += numberString[i];
+        }
+    }
+    const newNumber = Number(start);
+    return newNumber;
+};
+
+reverseInt(3100);
