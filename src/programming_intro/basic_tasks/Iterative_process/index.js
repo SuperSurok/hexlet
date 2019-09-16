fd4d030a8b1f47f8884243f044107c67ed480fe4
Lebedev Sergey <oddyone@mail.ru>
2019-09-16 12:31:47 +0300

    Iterative process.

---
+++ src/programming_intro/basic_tasks/Iterative_process/index.js
@@ -0,0 +1,67 @@
+///////////////////////////////////////
+///// The Very First Solution /////////
+///////////////////////////////////////
+
+{
+    const smallestDivisor = (num) => {
+        // BEGIN (write your solution here)
+        function process(num, divisor) {
+            if (num === 1) {
+                return 1;
+            } else if (num === 0) {
+                return 0;
+            } else {
+                if (num % divisor === 0) {
+                    return divisor;
+                } else {
+                    if (divisor === num)
+                        return num;
+                    return process(num, ++divisor);
+                }
+            }
+        }
+        return process(num, 2);
+        // END
+    };
+}
+///////////////////////////////////////
+///// More Adult Solution /////////////
+///////////////////////////////////////
+{
+    const smallestDivisor = (num) => {
+        // BEGIN (write your solution here)
+        const iter = (number, acc) => {
+            if (num === 1) return num;
+            if (number % acc === 0 || number === acc) return acc;
+            return iter(number, acc + 1);
+        };
+        return iter(num, 2);
+        // END
+    };
+}
+
+///////////////////////////////////////
+///// Master Solution /////////////////
+///////////////////////////////////////
+{
+    const smallestDivisor = (num) => {
+        // BEGIN
+        // Visualize Execution: https://goo.gl/Mc68MA
+        const iter = (acc) => {
+            // We use 'num / 2' in the condition below, and not 'num'.
+            // This is a simple optimization: a number cannot be divided
+            // by a number larger than its half.
+            if (acc > num / 2) {
+                return num;
+            }
+            if (num % acc === 0) {
+                return acc;
+            }
+            return iter(acc + 1);
+        };
+
+        return iter(2);
+        // END
+    };
+
+}
