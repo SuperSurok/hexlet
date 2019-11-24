a4bc8ef2881087c98b3e795c020da1c945a7d667
Lebedev Sergey <oddyone@mail.ru>
2019-11-24 20:02:48 +0300

    JS Array Course. Aggregation task completed.

---
+++ src/js_arrays/aggreagtion/calculateSum.js
@@ -0,0 +1,47 @@
+////////////////////////////////////////
+//////// First Solution ////////////////
+////////////////////////////////////////
+{
+    const calculateSum = (arr) => {
+        if (arr.length === 0) return null;
+        let sum = 0;
+        for (const num of arr) {
+            if (num % 3 === 0) {
+                sum += num;
+            }
+        }
+        return sum;
+    }
+}
+
+////////////////////////////////////////
+//////// Second Solution ///////////////
+////////////////////////////////////////
+{
+    const calculateSum = (arr) => {
+        if (arr.length === 0) return null;
+        let sum = 0;
+        for (const num of arr) {
+            sum = (num % 3 === 0) ? sum += num : sum += 0;
+        }
+        return sum;
+    }
+}
+
+////////////////////////////////////////
+//////// Master Solution ///////////////
+////////////////////////////////////////
+{
+    const sunThree = (coll) => {
+        if (coll.length === 0) {
+            return null;
+        }
+        let sum = 0;
+        for (const item of coll) {
+            if (item % 3 === 0) {
+                sum += item;
+            }
+        }
+        return sum;
+    };
+}
