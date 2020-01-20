bb729cd243ec2a48ef786d9511684a172be506b3
Sergey Lebedev <oddyone@mail.ru>
2020-01-20 12:53:40 +0300

    JS Object. Updated 'Destructuring' theory and added one more practice solution.

--- src/frontend_pro/js_objects/practice/destructuring/theory/index.js
+++ src/frontend_pro/js_objects/practice/destructuring/theory/index.js
@@ -1,11 +1,29 @@
-const options = { enabled: true, compression: {algo: 'gzip'} };
-const { enabled, compression } = options;
+// Извлечние значений по определённым ключам
+const person = {first: 'Rasmus', last: 'Lerdorf', manager: 'true'};
 
-const x = { o: [1, 2, 3] };
-const { o: [a, b, c] } = x;
+// Сохранение значений в константах firstName, lastName, manager
+const {first: firstName, last: lastName, manager: position} = person;
+console.log(firstName, lastName, position);
 
-const y = { o: [[1, 2, 3], { what: 'WHAT' }] };
-const { o: [[one, two, three], { what }] } = y;
+///// ===== Базовый синтаксис ===== /////
+// ключевое слово {что записывать : куда записывать}
+
+// Если имя переменной совпадает с фактическим значением ключа объекта,
+// запись можно сократить, указав только имя
+const {first, last, manager} = person;
+console.log(first, last, manager);
+
+// Вложенная деструктуризация
+const options = {enabled: true, compression: {algo: 'gzip'}};
+const {enabled, compression: {algo: compressionAlgo}} = options;
+console.log(compressionAlgo);
+
+// Комбинация с деструктуризацией массива
+const x = {o: [1, 2, 3]};
+const {o: [a, b, c]} = x;
+
+const y = {o: [[1, 2, 3], {what: 'WHAT'}]};
+const {o: [[one, two, three], {what}]} = y;
 console.log(one);
 
 const arr = [1, 2, 3];
