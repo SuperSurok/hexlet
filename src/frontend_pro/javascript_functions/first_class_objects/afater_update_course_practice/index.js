b6eeb0c8a57eab0843218f36d3eae815e165ba17
Lebedev Sergey <oddyone@mail.ru>
2020-01-23 11:47:34 +0300

    Course JS Functions. 'First object class' task completed.

---
+++ src/frontend_pro/javascript_functions/first_class_objects/afater_update_course_practice/index.js
@@ -0,0 +1,59 @@
+////////////////////////////////////////
+//////// First Solution ////////////////
+////////////////////////////////////////
+{
+    const run = (text) => {
+        const takeLast = (string, n) => {
+            const textLength = string.length;
+            let result = '';
+            if (textLength === 0 || n > textLength) return null;
+            for (let i = textLength - 1; i >= 0; i -= 1) {
+                result = `${result}${string[i]}`;
+            }
+            return result.substr(0, n);
+        };
+        return takeLast(text, 4);
+    };
+}
+
+////////////////////////////////////////
+//////// Second Solution ///////////////
+////////////////////////////////////////
+{
+    const run = (text) => {
+        const takeLast = (string, n) => {
+            const textLength = string.length;
+            if (textLength === 0 || textLength < n) return null;
+            let result = '';
+            for (let i = textLength - 1; result.length < n; i -= 1) {
+                result = `${result}${string[i]}`;
+            }
+            return result;
+        };
+        return takeLast(text, 4)
+    };
+}
+
+////////////////////////////////////////
+//////// Master Solution ///////////////
+////////////////////////////////////////
+{
+    const run = (text) => {
+        // BEGIN
+        const takeLast = (str, length) => {
+            if (str === '' || str.length < length) {
+                return null;
+            }
+
+            let result = '';
+            for (let i = str.length - 1; result.length < length; i -= 1) {
+                result = `${result}${str[i]}`;
+            }
+K
+            return result;
+        };
+        // END
+
+        return takeLast(text, 4);
+    };
+}
