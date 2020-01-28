7a4492e7628c74e448662686a507632c48b6a701
Lebedev Sergey <oddyone@mail.ru>
2020-01-28 23:05:10 +0300

    Course JS Functions. 'Signals' task completed.

---
+++ src/frontend_pro/javascript_functions/signals/theory/index.js
@@ -0,0 +1,8 @@
+const getJsFileNames = (paths) => paths
+  .filter((filepath) => fs.existsSync(filepath)) // отбираем реально существующие файлы
+  .filter((filepath) => fs.lstatSync(filepath).isFile()) // отбор по типу файла
+  .filter((filepath) => path.extname(filepath).toLowerCase() === '.js') // отбор по расширению
+  .map((filepath) => path.basename(filepath, extension)); // отображаем в имена (нам нужен массив с именами)
+
+const names = getJsFileNames(['index.js', 'wop.JS', 'nonexists', 'node_modules']);
+console.log(names); // => [index, wop]
