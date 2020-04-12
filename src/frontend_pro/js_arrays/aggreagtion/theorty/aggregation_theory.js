92e61748c24fe2a6d69c972d4968cf9fa93649a3
Lebedev Sergey <oddyone@mail.ru>
2020-04-12 20:17:35 +0300

    Course 'JS Arrays'. Added theory for Aggregation lesson.

---
+++ src/frontend_pro/js_arrays/aggreagtion/theorty/aggregation_theory.js
@@ -0,0 +1,24 @@
+// get max
+const getMax = (arr) => {
+    if (arr.length === 0) return null;
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i += 1) {
+        const currentIndex = arr[i];
+        if (currentIndex > max) {
+            max = currentIndex;
+        }
+    }
+    return max;
+}
+
+// get sum
+const getSum = (arr) => {
+    let sum = 0;
+    for (const num in arr) {
+        sum += arr[num];
+    }
+    return sum;
+}
+
+const aarr = [1, 2, 3];
+console.log(getSum(aarr))
