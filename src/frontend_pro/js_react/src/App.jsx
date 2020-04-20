84b7f14cb60e09e9ba503799524ac650bcbb5c6a
Lebedev Sergey <oddyone@mail.ru>
2020-04-20 10:11:30 +0300

    Course 'JS React'. Props lesson completed.

--- src/frontend_pro/js_react/src/App.jsx
+++ src/frontend_pro/js_react/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Card from './practice/components/Card';
 import Jsx from './practice/jsx/Jsx';
+import PropsLesson from "./practice/props/Props";
 class App extends React.Component {
     render() {
+        const titlePropsLesson = 'Title Props Lesson';
+        const textPropsLesson = 'Text Prop Lesson'
         return (
             <div className='container'>
                 <Card />
                 <Jsx />
+                <PropsLesson titlePropsLesson={textPropsLesson} textPropsLesson={textPropsLesson}/>
             </div>
         );
     }
