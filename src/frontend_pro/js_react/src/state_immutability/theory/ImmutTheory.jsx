914bbd48cb372fa16caa63203ba066d34f51e2f5
Sergey Lebedev <oddyone@mail.ru>
2020-05-17 08:51:28 +0300

    'JS React' course. Start Immutability lesson.

--- src/frontend_pro/js_react/src/state_immutability/theory/ImmutTheory.jsx
+++ src/frontend_pro/js_react/src/state_immutability/theory/ImmutTheory.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class ImmutTheory extends Component {
   constructor(props) {
     super(props);
-    const items = [1, 2, 3, 4, 5].map(i => ({ id: i }));
+    const items = [1, 2, 3, 4, 5].map((i) => ({ id: i }));
     this.state = {
       items,
     };
@@ -13,8 +13,16 @@ class ImmutTheory extends Component {
     e.preventDefault();
     const { items } = this.state;
     const newItems = items.filter((item) => item.id !== id);
-    this.setState({ items: newItems });
+    this.setState({
+      items: newItems,
+    });
   };
+
+  renderItem = ({ id }) => (
+    <li key={id} onClick={this.removeItem(id)}>
+      <a href="#">{id}</a>
+    </li>
+  );
 
   render() {
     const { items } = this.state;
@@ -23,15 +31,6 @@ class ImmutTheory extends Component {
         <h4 className="title ml-3">Delete Item Array</h4>
         <ul>{items.map((item) => this.renderItem(item))}</ul>
       </div>
-    );
-  }
-  renderItem({ id }) {
-    return (
-      <li>
-        <a href="#" onClick={this.removeItem(id)}>
-          {id}
-        </a>
-      </li>
     );
   }
 }
