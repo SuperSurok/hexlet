33bb4ed92d469d8422ffbb819731977d805a65fd
Sergey Lebedev <oddyone@mail.ru>
2020-07-16 18:43:11 +0300

    Hexlet. JS OOP Intro. Class practice was completed.

---
+++ src/frontend_pro/js_oop_intro/__tests__/class_practice.test.js
@@ -0,0 +1,21 @@
+import Cart from '../class/practice/class_practice'
+
+test('Cart', () => {
+    const cart = new Cart();
+    expect(cart.getItems()).toHaveLength(0);
+
+    cart.addItem({name: 'car', price: 3}, 5);
+    const expectedItems = [{good: {name: 'car', price: 3}, count: 5}];
+    expect(cart.getItems()).toStrictEqual(expectedItems);
+    expect(cart.getCost()).toBe(15);
+    expect(cart.getCount()).toBe(5);
+
+    cart.addItem({name: 'house', price: 10}, 2);
+    const expectedItems2 = [
+        {good: {name: 'car', price: 3}, count: 5},
+        {good: {name: 'house', price: 10}, count: 2},
+    ];
+    expect(cart.getItems()).toStrictEqual(expectedItems2);
+    expect(cart.getCost()).toBe(35);
+    expect(cart.getCount()).toBe(7);
+});
